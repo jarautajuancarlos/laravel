9bab19b80407643535dd6bd9c39917a4343d0080
JuanCarlos <jarauta.juancarlos@gmail.com>
2021-04-26 15:27:24 +0200

    controller ejemplo

---
+++ app/Http/Controllers/Ejemplo_controller.php
@@ -0,0 +1,16 @@
+<?php
+
+// controlador para gestionar las rutas
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// use Illuminate\Foundation\Bus\DispatchesJobs;
+// use Illuminate\Foundation\Validation\ValidatesRequests;
+// use Illuminate\Routing\Controller as BaseController;
+
+class Ejemplo_controller extends Controller{
+    public function inicio(){
+      return "Estás en el inicio del sitio";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-//..................................... empezamos a probar
+//....................................codigo inicial
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return "estas en el home";
-});
-Route::get('/quiensoy', function () {
-    return "estas en quien somos";
-});
 
-// .........................................pasar por url parametros
-Route::get('/post/{id}', function ($id) {
-    return "Este es el post nº " . $id;
-});
+// enlace ejemplo_controller
+Route::get('/inicio', 'Ejemplo_controller@inicio');
 
-// .........................................pasar por url varios parametros
-Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
-    return "Este es el post nº " . $id . " creado por: " . $nombre;
-});
-
-// ...................................pasar por url con expresiones regulares
-Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
-    return "Este es el post nº " . $id . " creado por: " . $nombre;
-
-})->where('nombre', '[aA-zZ]+');
-
-//....................................codigo inicial
+//..................................... empezamos a probar
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+// Route::get('/home', function () {
+//     return "estas en el home";
+// });
+// Route::get('/quiensoy', function () {
+//     return "estas en quien somos";
+// });
+
+// .........................................pasar por url parametros
+// Route::get('/post/{id}', function ($id) {
+//     return "Este es el post nº " . $id;
+// });
+
+// .........................................pasar por url varios parametros
+// Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
+//     return "Este es el post nº " . $id . " creado por: " . $nombre;
+// });
+
+// ...................................pasar por url con expresiones regulares
+// Route::get('/post/{id}/{nombre}', function ($id, $nombre) {
+//     return "Este es el post nº " . $id . " creado por: " . $nombre;
+//
+// })->where('nombre', '[aA-zZ]+');
